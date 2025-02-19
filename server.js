d0fbd06b9ceb37b2c7702fd840874f0cdea3b8ca
Esrbwt1 <esrbwt20@gmail.com>
2025-02-19 18:10:09 +0300

    Initial project setup with basic Express server

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// A simple test route
+app.get('/', (req, res) => {
+  res.send('Welcome to Cognify Network MVP!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
